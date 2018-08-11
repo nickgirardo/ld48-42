008746e4d8ed8869f309d6c029335ffa2704b205
Nick Girardo <nickgirardo@gmail.com>
2018-08-10 21:33:34 -0400

    Canvas made, resize set up

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 
-console.log('hello!');
+const canvas = document.querySelector('canvas#ld48');
+const ctx = canvas.getContext('2d');
+
+function resize() {
+  canvas.width = Math.min(window.innerHeight, window.innerWidth);
+  canvas.height = Math.min(window.innerHeight, window.innerWidth);
+}
+
+function init() {
+  resize()
+  window.addEventListener('resize', resize);
+}
+
+init();
